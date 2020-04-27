09ceab093ad06a5cd50e35d52f43ceb2bc2f63d2
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-04-28 00:44:03 +0100

    filter by color, category or/and price

--- js/filter.js
+++ js/filter.js
@@ -1,5 +1,6 @@
 const sortOptionsContainer = document.querySelector("#sort-options")
 const sortOptionInputs = sortOptionsContainer.querySelectorAll("input")
+const slideContainer = document.querySelector("#sliderBox")
 
 const createFilterObject = () => {
     let from = parseInt(sliderLeft.value)
@@ -26,41 +27,50 @@ const createFilterObject = () => {
     }
 }
 
-
 const applyFilters = (priceFrom, priceTo, selectedColors, selectedCategories) => {
     console.log("APPLY FILTERS", priceFrom, priceTo, selectedColors, selectedCategories)
 
     const filteredProducts = PRODUCTS.filter(product => {
-        // return true or false
+        //return true or false
         if (product.price >= priceFrom && product.price <= priceTo) {
             return true
         } else {
             return false
         }
     }).filter(product => {
-        // return true or false
-        if (selectedColors.length === 0 || selectedColors.includes(product.color)){
+        if (selectedColors.length === 0 || selectedColors.some(color => product.colors.includes(color))){
             return true
         } else {
             return false
         }
-    }).filter(product => {
-        if (selectedCategories.length === 0 || selectedCategories.includes(product.category)) {
+     }).filter(product => {
+        if (selectedCategories.length === 0 || selectedCategories.includes(product.type)) {
             return true
         } else {
             return false
         }
     })
-
     renderProducts(filteredProducts)
 }
 
+colorContainer.addEventListener('change', () => {
+    const filterObject = createFilterObject()
+    applyFilters(filterObject.from, filterObject.to, filterObject.selectedColors, filterObject.selectedCategories)
+})
+categoryContainer.addEventListener('change', () => {
+    const filterObject = createFilterObject()
+    applyFilters(filterObject.from, filterObject.to, filterObject.selectedColors, filterObject.selectedCategories)
+})
+slideContainer.addEventListener('change', () => {
+    const filterObject = createFilterObject()
+    applyFilters(filterObject.from, filterObject.to, filterObject.selectedColors, filterObject.selectedCategories)
+})
 
 
 const applySortOption = (sortOption) => {
     if (sortOption === "DEFAULT") {
-        PRODUCTS.sort((a, b) => {
-            return a.id - b.id
+        PRODUCTS.forEach((product) => {
+            return product
         })
     } else if (sortOption === "PRICE_ASC") {
         PRODUCTS.sort((a, b) => {
